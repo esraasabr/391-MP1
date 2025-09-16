128794021af3d7028a8baff9410a6c07b5024487
esraasabr <157326699+esraasabr@users.noreply.github.com>
2025-09-16 01:37:02 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,67 @@
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let output = document.getElementById("output");
+
+function doAdd() {
+    output.innerHTML = String(Number(one.value) + Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkgreen";
+    }
+}
+function doSubtract() {
+    output.innerHTML = String(Number(one.value) - Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkgreen";
+    }
+}
+function doMultiply() {
+    output.innerHTML = String(Number(one.value) * Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkgreen";
+    }
+}
+function doDivide() {
+    output.innerHTML = String(Number(one.value) / Number(two.value));
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkgreen";
+    }
+}
+function doPower() {
+    let result= 1;
+    for (let i=0; i<Number(two.value); i++){
+        result*=Number(one.value);
+    }
+   output.innerHTML= String(result);
+    if(Number(output.innerHTML) <0){
+        // output.innerHTML="&#127773;";
+        output.style.color="red";
+    }
+    else{
+        output.style.color= "darkgreen";
+    }
+}
+
+function doClear(){
+    one.value="";
+    two.value="";
+    output.innerHTML="";
+}
+
+
+
